e238f85da9f1406415741f0d88878c177edd4d4d
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-12-04 17:32:09 +0100

    Update pythagorean_triplet.py

--- pythagorean_triplet.py
+++ pythagorean_triplet.py
@@ -2,8 +2,8 @@
 
 def triplets_with_sum(number):
     rslt = []
-    for a in range(1, int(number/3)):
-        for b in range(a, int(number/2)):
+    for a in range(1, number//3):
+        for b in range(a, number//2):
             c = number - a - b
             if ((a**2 + b**2) == c**2):
                 rslt.append([a, b, c])
